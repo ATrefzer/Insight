79f9660d7173ec1d42213671fd8cd06dc4891767
ATrefzer <36333177+ATrefzer@users.noreply.github.com>
2018-03-30 21:29:56 +0200

    Moved common code to base class GitProviderBase

--- Insight/Dto/DataGridFriendlyArtifactBasic.cs
+++ Insight/Dto/DataGridFriendlyArtifactBasic.cs
@@ -1,14 +1,17 @@
 ﻿namespace Insight.Dto
 {
-    public class DataGridFriendlyArtifactBasic
+    /// <summary>
+    /// DO NOT FORMAT
+    /// </summary>
+    public sealed class DataGridFriendlyArtifactBasic
     {
-        public int CodeAge_Days { get; set; }
+        public string Revision { get; set; }
+        public string LocalPath { get; set; }
         public int Commits { get; set; }
         public int Committers { get; set; }
-        public double Hotspot { get; internal set; }
-        public int LOC { get; set; }
-        public string LocalPath { get; set; }
-        public string Revision { get; set; }
         public int WorkItems { get; set; }
+        public int LOC { get; set; }
+        public double Hotspot { get; internal set; }
+        public int CodeAge_Days { get; set; }
     }
 }
